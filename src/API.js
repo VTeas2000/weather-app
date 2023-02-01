d540c7b19259412f263bb590a464810ef6e0dfcc
VTeas2000 <vteas2000@gmail.com>
2023-02-01 14:32:14 -0800

    init

---
+++ src/API.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0892d5cf50msha29ea2913d8a863p1cd37fjsnb83d7fef4841', //my GeoDB Cities API key
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "bfb0268f6dcf52ee2b3b684f32d520b3"; //my OpenWeather API key
